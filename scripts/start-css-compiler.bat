5de2c0163deade15b852f28a233a62fc650bbb26
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-22 09:23:48 -0500

    add css compiler script and file

---
+++ scripts/start-css-compiler.bat
@@ -0,0 +1,8 @@
+:: --------------------------------------------
+:: Start up the sass compiler
+:: --------------------------------------------
+
+cd C:\xampp\htdocs\files\tickle\src\tickle\gui\static\css
+
+sass --watch style.scss style.css
+
