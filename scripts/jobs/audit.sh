e4df710d21469e8022552302430716577894574a
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-26 15:21:43 -0500

    Update audit.sh

--- scripts/jobs/audit.sh
+++ scripts/jobs/audit.sh
@@ -1,11 +1,12 @@
 cd /var/www/tickle/scripts/jobs
-source .constants.sh
+# source .constants.sh
+# echo 'start'
+# echo $PATH_APPLICATION
+# cd $PATH_APPLICATION
 
+ls
 
-echo 'start'
-echo $PATH_APPLICATION
-
-cd $PATH_APPLICATION
+echo 'inside'
 
 python3.8 -m tickle.jobs.audit.main
 
