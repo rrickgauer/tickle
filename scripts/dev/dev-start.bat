75f0639919076d8643e9bc604f326b1e4b02ca3b
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-26 12:59:28 -0500

    Update dev-start.bat

--- scripts/dev/dev-start.bat
+++ scripts/dev/dev-start.bat
@@ -2,5 +2,5 @@
 :: Startup all the development servers/compilers
 :: --------------------------------------------
 
-cd C:\xampp\htdocs\files\tickle\scripts
+cd C:\xampp\htdocs\files\tickle\scripts\dev
 wt -M start-api-server.bat ; start-gui-server.bat ; start-css-compiler.bat ; start-js.bat
