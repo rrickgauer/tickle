e889f42f3c572268dfb5334810e01be218e2adfb
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-26 21:55:16 +0100

    update

--- scripts/jobs/audit.sh
+++ scripts/jobs/audit.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-cd /var/www/tickle/src/tickle/jobs
-cd "$(dirname "$0")";
-CWD="$(pwd)"
-echo $CWD
-#python -m /var/www/tickle/scripts/jobs.py
-python -m tickle.jobs.audit.main
+cd /var/www/tickle/src
+/usr/local/bin/python3.8 -m tickle.jobs.audit.main
