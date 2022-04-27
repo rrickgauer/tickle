8bb6a5f7a0df346fe43632352c4f7f5038e1c286
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-27 14:12:53 -0500

    Update test.py

--- src/tickle/api/routes/test.py
+++ src/tickle/api/routes/test.py
@@ -11,6 +11,8 @@ import flask
 from tickle.common import responses
 from tickle.api import security
 from tickle import stockslib
+from tickle.common.utilities import dumpJson
+import investpy
 
 
 # module blueprint
@@ -44,3 +46,24 @@ def testInvestpy5():
     tags = stockslib.test_tags.TEST_TAGS
     prices = stockslib.routines.getPrices(tags)
     return responses.get(prices)
+
+
+#------------------------------------------------------
+# Test investpy
+#------------------------------------------------------
+@bp_test.route('investpy/<test_converter>')
+def testConverter(test_converter):
+
+
+    output = dict(
+        # crypto_search = investpy.crypto.search_cryptos('symbol', 'DOGE').to_dict('records'),
+        crypto_dict = investpy.crypto.get_cryptos_dict()[:20],
+        # stocks_list = investpy.stocks.get_stocks_list()[:20],
+        stocks_dict = investpy.stocks.get_stocks_dict()[:20],
+    )
+
+
+    return flask.jsonify(output)
+    
+    dumpJson(flask.request.args.to_dict())
+    return str(test_converter)
