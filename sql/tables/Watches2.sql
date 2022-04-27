7d157f1cd3adcb8cffaad14fadf686be6be8b0c3
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-27 14:44:42 -0500

    add the 2 new database tables

---
+++ sql/tables/Watches2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Watches2
+(
+    id CHAR(36) NOT NULL UNIQUE,
+    pair_type INT UNSIGNED NOT NULL,
+    pair_id INT UNSIGNED NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    watch_type INT UNSIGNED NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    closed_on TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (pair_type) REFERENCES Pair_Types(id) ON UPDATE CASCADE,
+    FOREIGN KEY (watch_type) REFERENCES Watch_Types (id) ON UPDATE CASCADE
+);
