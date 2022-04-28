8952b5d56efdb59906df5eb83d5463b10eb9bb60
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-28 16:42:54 -0500

    add redirect

--- src/tickle/gui/routes/home.py
+++ src/tickle/gui/routes/home.py
@@ -1,24 +1,21 @@
 """
 ********************************************************************************************
 
-Url Prefix: /new
+Url Prefix: /
 
 ********************************************************************************************
 """
 
 from __future__ import annotations
 import flask
-from tickle.common.domain.enums.watches import WatchTypes
-from tickle.gui.services import home_page as homepage_services
 
 
 # module blueprint
-bp_new = flask.Blueprint('new', __name__)
+bp_home = flask.Blueprint('home', __name__)
 
 #------------------------------------------------------
 # Get ticker information
 #------------------------------------------------------
-@bp_new.route('')
-def new():
-    return flask.render_template('new/new-watch.html')
-
+@bp_home.route('')
+def homePage():
+    return flask.redirect(flask.url_for('new.new'))
