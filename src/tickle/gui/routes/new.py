ec8ba4d49a7ad81c6cab517d1512a0a61afa4e26
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-28 15:03:56 -0500

    make a new template with all the form elements on it

--- src/tickle/gui/routes/new.py
+++ src/tickle/gui/routes/new.py
@@ -8,7 +8,7 @@ Url Prefix: /new
 
 from __future__ import annotations
 import flask
-from tickle.common.domain.enums.watches import TickerTypes, WatchTypes
+from tickle.common.domain.enums.watches import WatchTypes
 from tickle.gui.services import home_page as homepage_services
 
 
@@ -19,35 +19,42 @@ bp_new = flask.Blueprint('new', __name__)
 # Get ticker information
 #------------------------------------------------------
 @bp_new.route('')
-def getTickerInfo():
-    payload = dict(
-        url_values = homepage_services.getUrlValues(),
-    )
+def new():
+    return flask.render_template('new/new-watch.html')
 
-    return flask.render_template('new/form-sections/ticker.html', data=payload)
+# #------------------------------------------------------
+# # Get ticker information
+# #------------------------------------------------------
+# @bp_new.route('')
+# def getTickerInfo():
+#     payload = dict(
+#         url_values = homepage_services.getUrlValues(),
+#     )
 
-
-#------------------------------------------------------
-# Get price info
-#------------------------------------------------------
-@bp_new.route('<ticker_type:ticker_type>/<string:ticker>')
-def getPriceAndWatchType(ticker_type: TickerTypes, ticker: str):
-
-    payload = dict(
-        url_values = homepage_services.getUrlValues(),
-    )
-
-    return flask.render_template('new/form-sections/price.html', data=payload)
+#     return flask.render_template('new/form-sections/ticker.html', data=payload)
 
 
-#------------------------------------------------------
-# Get the email
-#------------------------------------------------------
-@bp_new.route('<ticker_type:ticker_type>/<ticker>/<price>/<watch_type:watch_type>')
-def getEmail(ticker_type: TickerTypes, ticker, price, watch_type: WatchTypes):
+# #------------------------------------------------------
+# # Get price info
+# #------------------------------------------------------
+# @bp_new.route('<ticker_type:ticker_type>/<string:ticker>')
+# def getPriceAndWatchType(ticker_type: TickerTypes, ticker: str):
 
-    payload = dict(
-        url_values = homepage_services.getUrlValues(),
-    )
+#     payload = dict(
+#         url_values = homepage_services.getUrlValues(),
+#     )
 
-    return flask.render_template('new/form-sections/email.html', data=payload)
+#     return flask.render_template('new/form-sections/price.html', data=payload)
+
+
+# #------------------------------------------------------
+# # Get the email
+# #------------------------------------------------------
+# @bp_new.route('<ticker_type:ticker_type>/<ticker>/<price>/<watch_type:watch_type>')
+# def getEmail(ticker_type: TickerTypes, ticker, price, watch_type: WatchTypes):
+
+#     payload = dict(
+#         url_values = homepage_services.getUrlValues(),
+#     )
+
+#     return flask.render_template('new/form-sections/email.html', data=payload)
