19e6967b444b039ed9b351488a6e69b7022b638d
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-29 10:16:42 -0500

    combine both view routes into 1 module

--- src/tickle/gui/routes/__init__.py
+++ src/tickle/gui/routes/__init__.py
@@ -1,3 +1,2 @@
 from .api import bp_api as bp_api
-from .new import bp_new as bp_new
-from .home import bp_home as bp_home
+from .views import bp_views as bp_views 
---
+++ src/tickle/gui/routes/views.py
@@ -0,0 +1,35 @@
+"""
+********************************************************************************************
+
+Url Prefix: /
+
+These are the routes for the html pages.
+
+********************************************************************************************
+"""
+
+from __future__ import annotations
+import flask
+
+
+# module blueprint
+bp_views = flask.Blueprint('views', __name__)
+
+#------------------------------------------------------
+# Home page
+#
+# tickle.com
+#------------------------------------------------------
+@bp_views.route('')
+def home():
+    return flask.render_template('home/index.html')
+
+
+#------------------------------------------------------
+# Create a new watch
+#
+# tickle.com/new
+#------------------------------------------------------
+@bp_views.route('new')
+def newWatch():
+    return flask.render_template('new/index.html')
