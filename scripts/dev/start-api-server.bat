be683bed277336a41259759d86bd9263f76dfd30
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-05-11 08:41:32 -0500

    create a virtual enviornment

--- scripts/dev/start-api-server.bat
+++ scripts/dev/start-api-server.bat
@@ -2,10 +2,10 @@
 :: Start up the api development flask server
 :: --------------------------------------------
 
-@REM cd C:\xampp\htdocs\files\tickle\src\tickle\api
 cd C:\xampp\htdocs\files\tickle\src
 
+set flask_program=C:\xampp\htdocs\files\tickle\venv\Scripts\flask
 set FLASK_ENV=development
 set FLASK_APP=tickle.api
 
-flask run --with-threads --host 0.0.0.0 --port 5010
+%flask_program% run --with-threads --host 0.0.0.0 --port 5010
--- scripts/dev/start-gui-server.bat
+++ scripts/dev/start-gui-server.bat
@@ -7,4 +7,6 @@ cd C:\xampp\htdocs\files\tickle\src
 set FLASK_ENV=development
 set FLASK_APP=tickle.gui
 
-flask run --with-threads --host 0.0.0.0 --port 5011
+set flask_program=C:\xampp\htdocs\files\tickle\venv\Scripts\flask
+
+%flask_program% run --with-threads --host 0.0.0.0 --port 5011
