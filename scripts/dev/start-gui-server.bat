3f0ad7dec9bbc477cdcd8f898bcc8a211ae3a007
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-05-12 12:46:13 -0500

    Branch virtualenv (#71)
    * create a virtual enviornment
    
    * Update requirements.txt
    
    * update the dev scripts to use '.venv'

--- scripts/dev/start-gui-server.bat
+++ scripts/dev/start-gui-server.bat
@@ -4,7 +4,8 @@
 
 cd C:\xampp\htdocs\files\tickle\src
 
+set flask_program=C:\xampp\htdocs\files\tickle\.venv\Scripts\flask
 set FLASK_ENV=development
 set FLASK_APP=tickle.gui
 
-flask run --with-threads --host 0.0.0.0 --port 5011
+%flask_program% run --with-threads --host 0.0.0.0 --port 5011
