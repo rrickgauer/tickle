cf42eeefd2bd673174fbb91c7a9d45cf6080f769
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-05-12 14:09:32 -0500

    add about page

--- src/tickle/common/config/configs.py
+++ src/tickle/common/config/configs.py
@@ -23,8 +23,8 @@ class Dev(ConfigBase):
     SECRET_KEY_API = b'2JWFQU+LZXsQIiTef21hFMxQ2gdEE2YQwvkd8FztLwUYOVXzSxwuP+tTXenvhOIZ96iowjwSToqQLEU4rUCUBg=='
     SECRET_KEY_GUI = b'73fT427SmZ/TQhl7lVRvd2fSMHW+LdNPxfS+2xHtn9wjCzSy7OouBvsgoQHbtlfoTUiJUASknvIDWZ35qqoF5g=='
     
-    URL_API = 'http://10.0.0.82:5010/v1'
-
+    URL_API = 'http://127.0.0.1:5010/v1'
+    
     DB_NAME     = 'Tickle_Dev'
     DB_HOST     = '104.225.208.163'
  
